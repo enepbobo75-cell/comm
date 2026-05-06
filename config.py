9487c821a267738daefcdca9017c6a194bb7a6d8
enepbobo75-cell <enepbobo75@gmail.com>
2026-05-06 00:13:30 +0000

    Config base de données Render

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
 import psycopg2
+import os
 
 def get_connection():
-    conn = psycopg2.connect(
-        host="localhost",
-        database="ECOLE",
-        user="postgres",
-        password="postgres123"
-    )
+    database_url = os.environ.get('DATABASE_URL', 
+        'postgresql://commerce_6snn_user:cRU7C4jx204dTe3vhIDaBW4c9FveiwXA@dpg-d7t8ae1kh4rs73df2t3g-a.ohio-postgres.render.com/commerce_6snn')
+    conn = psycopg2.connect(database_url)
     return conn
-
-
